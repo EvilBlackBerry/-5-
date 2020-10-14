0fa5407345acd19afeed0e8c05fcc6ff2cfa9295
EvilBlackBerry <72876483+EvilBlackBerry@users.noreply.github.com>
2020-10-14 21:26:42 +0500

    Add files via upload

---
+++ main (2).swift
@@ -0,0 +1,9 @@
+let x = 1000000.0
+var y = 0.05
+var x1 = x * y + x
+let x2 = x1 * y + x1
+let x3 = x2 * y + x2
+let x4 = x3 * y + x3
+let x5 = x4 * y + x4
+
+print ("Сумма вклада через 5 лет = \(x5)")
